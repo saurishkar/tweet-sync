d37d3d1c6a9665de88b028dcf69c3e53ac9f1c81
Saurish Kar <basicallysaurish@gmail.com>
2023-04-07 23:26:41 +0530

    added change password feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/about", to: "about#index"
 
+  get "/change_password", to: "password#edit"
+  patch "/change_password", to: "password#update"
+
   get "/signup", to: "registrations#new"
   post "/signup", to: "registrations#create"
 
