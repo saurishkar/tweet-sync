0de77c451b32781780be2da28dbb4aebca44a62f
Saurish Kar <basicallysaurish@gmail.com>
2023-07-19 11:03:52 +0530

    added listing and disconnect feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-    before_action :set_current_user
+  before_action :set_current_user
 
-    def set_current_user
-        if session[:user_id]
-            Current.user = User.find_by(id: session[:user_id])
-        end
+  def set_current_user
+    if session[:user_id]
+      Current.user = User.find_by(id: session[:user_id])
     end
+  end
 
-    def redirect_guest_to_signin
-        redirect_to signup_path, alert: "You need to be signed in to access this page" if !Current.user.present?
-    end
+  def redirect_guest_to_signin
+    redirect_to signup_path, alert: "You need to be signed in to access this page" if !Current.user.present?
+  end
 end
