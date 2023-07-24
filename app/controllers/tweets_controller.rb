4ab1bb9a13f77f61aa7f0887ebc9c0ffb575ec81
Saurish Kar <basicallysaurish@gmail.com>
2023-07-24 23:22:07 +0530

    added edit feature

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -23,6 +23,19 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to @tweet, notice: 'Tweet has been successfully updated'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def tweet_params
